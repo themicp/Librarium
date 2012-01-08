4c7532dead35859f2640f0b32ab59fa9ac0cc83c
Themis Papameletiou <themicp@gmail.com>
2012-01-08 00:59:31 +0000

    Remove useless files

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-    mysql_connect( "localhost", "librarium", "librarium" ) || $return = false;
-    mysql_select_db( "librarium" ) || $return = false;
-    mysql_query( 'SET NAMES utf8' ) || $return = false;
-?>
