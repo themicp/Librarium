857301fe7aec75cb77956e1ecca0948184daf8da
Themis Papameletiou <themicp@gmail.com>
2014-03-23 18:20:20 +0200

    mysql_escape_string is deprecated

--- controllers/session.php
+++ controllers/session.php
@@ -2,8 +2,8 @@
     include 'models/session.php';
     class SessionController {
         public static function create( $user, $pass ) {
-            $user = mysql_escape_string( $user );
-            $pass = mysql_escape_string( $pass );
+            $user = mysql_real_escape_string( $user );
+            $pass = mysql_real_escape_string( $pass );
 
             echo Session::create( $user, $pass );
         }
